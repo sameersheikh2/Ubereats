c4d8570720a45cd948456fa67bb8a7d0b7bb005d
sameersheikh2 <sameersheikh819@gmail.com>
2022-07-27 20:39:23 +0530

    added headline cards with some food image cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import HeadlinCards from './components/HeadlinCards'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
@@ -7,6 +8,7 @@ const App = () => {
     <div>
       <Navbar/>
       <Hero/>
+      <HeadlinCards/>
     </div>
   )
 }
